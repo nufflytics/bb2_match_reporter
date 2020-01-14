90eda631a99f4478081d0c87720f8982194806f9
Stephen <36123293+sparce@users.noreply.github.com>
2020-01-15 10:15:38 +1100

    Add offseason to racial reporting

--- bb2_match_reporter.R
+++ bb2_match_reporter.R
@@ -742,7 +742,7 @@ post_match <- function(league_params, match_data, times = 0, check_clans = T, ch
     post_clan(league_params, match_data)
   }
   
-  if (check_race & any(str_detect(str_to_lower(match_data$match$leaguename), c("big o", "gman", "rel", "rebbl clan")))) {
+  if (check_race & any(str_detect(str_to_lower(match_data$match$leaguename), c("big o", "gman", "rel", "rebbl clan","off season")))) {
     post_race(league_params, match_data)
   }
   
